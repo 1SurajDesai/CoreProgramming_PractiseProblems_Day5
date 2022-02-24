4437efb9d3f3f2f116a01b5886e633ae009121df
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 12:56:21 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //VowelConstant.CheckVowelConstant();
+            //PrimeNo.CheckPrimeNo();
+            //SwappingNo.SwapTwoNo();
+            //FindLargestNo.FindLargestNumber();
+            //CoinFlip.FlipTheCoin();
+            //LeapYear.CheckLeapYear();
+            //PowerOftwo.powerFunction();
+            //Harmonic.harmonicFunction();
+            //PrimeFactors.FindPrimeFactors();
+            //EvenOdd.EvenOrOddNo();
+            //QuotientAndRemainder.FindQuotientAndRemainder();
+        }
+    }
+}
