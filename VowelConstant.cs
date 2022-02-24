fe55ccd902a68ee0e2abc248fdbd91374673fa72
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 13:17:57 +0530

    Adding program to check vowel or consonant

---
+++ VowelConstant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class VowelConstant
+    {
+        public static void CheckVowelConstant()
+        {
+            Console.WriteLine("Enter a character");
+            String str = Console.ReadLine().ToLower();
+            char ch = Convert.ToChar(str);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == '0' || ch == 'u')
+            {
+                Console.WriteLine("Its a Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Its a Constant");
+            }
+        }
+    }
+}
