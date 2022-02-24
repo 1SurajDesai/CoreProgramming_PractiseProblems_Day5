8e65496f4ceecbbddaeba8e36a7f7edb20c08459
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 13:09:36 +0530

    Adding program to get prime factors of number

---
+++ PrimeFactors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class PrimeFactors
+    {
+        public static void FindPrimeFactors()
+        {
+            int a, b, flag = 0;
+            Console.WriteLine("Please enter your integer: ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 2; b < a; b++)
+            {
+                if (a % b == 0)
+                {
+                    for (int i = 2; i < b; i++)
+                    {
+                        if (b % i == 0)
+                        {
+                            flag = 1;
+                            break;
+                        }
+
+                    }
+                    if (flag == 0)
+                        Console.WriteLine(b);
+                }
+            }
+        }
+    }
+}
