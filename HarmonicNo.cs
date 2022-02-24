72ecd30fcf0de4afb321d22a2c47cbb1cfa8d463
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 13:07:42 +0530

    Adding program to get harmonic number

---
+++ HarmonicNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class Harmonic
+    {
+        public static void harmonicFunction()
+
+        {
+            Console.WriteLine("Enter a number:");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            double x = 0.0;
+
+            for (double i = 1; i < n; i++)
+            {
+                x = x + 1 / i;
+            }
+            Console.WriteLine(x + " is harmonic value");
+        }
+
+    }
+}
